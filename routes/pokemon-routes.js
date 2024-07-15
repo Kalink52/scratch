2be1b25e0ba06e9e0f6a04530815e6b1b5c5908f
Kalink52 <micah.cox52@gmail.com>
2024-07-15 17:41:42 -0400

    i think it's ready?

--- routes/pokemon-routes.js
+++ routes/pokemon-routes.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
-const { Pokemon } = require('../models/pokemon')
+const Pokemon  = require('../models/pokemon')
 
 //path is  /pokemon
 
 router.get('/', async (req, res) => {
     try{
-        const pokemonData = await Pokemon.findAll()
-        console.log(pokemonData)
+        const pokemonData = await Pokemon.findAll();
         if (!pokemonData){
             res.status(400).json({message: "not found"})
         }
@@ -21,7 +20,6 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
     try{
         const pokemonData = await Pokemon.create(req.body)
-        console.log(pokemonData)
         res.status(200).json(pokemonData)
 
     } catch (err) {
